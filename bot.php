6f4434a4510cece811b0bb289d9b9b199ab89903
Nattapat <l3elieveOne@gmail.com>
2017-08-11 16:12:38 +0700

    Update bot file

--- bot.php
+++ bot.php
@@ -37,7 +37,7 @@ if (!is_null($events['events'])) {
 			
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => $messages,
 			];
 			
 			$post = json_encode($data);
